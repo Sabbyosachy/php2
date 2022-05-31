d2bdc2408451be2d519f3f34c03b08ebe54bf3b5
Sabbyosachy <subbyosachychakraborty@gmail.com>
2022-05-31 21:37:01 +0600

    first

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    
+<div class="container pt-5 d-flex justify-content-center">
+<form action="home.php" method="get">
+    <h3 class="text-center pb-5">Login</h3>
+<div class="mb-3">
+  Email:
+  <input type="email" class="form-control name="email" aria-describedby="emailHelp">
+</div>
+<div class="mb-3">
+  Password:
+  <input type="password" class="form-control name="password" >
+</div>
+<button type="submit" class="btn btn-primary w-100">Submit</button>
+</form>
+</div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
